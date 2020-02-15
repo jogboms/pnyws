c274fe80caddedb4758e6a660d80c3eadc6e1bb9
Jogboms <jeremiahogbomo@gmail.com>
2020-02-15 09:22:08 +0100

    Introduce HomePage

--- lib/coordinator/shared_coordinator.dart
+++ lib/coordinator/shared_coordinator.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:pnyws/constants/mk_routes.dart';
 import 'package:pnyws/coordinator/coordinator_base.dart';
+import 'package:pnyws/screens/home/home_page.dart';
 import 'package:pnyws/screens/splash/splash_page.dart';
 import 'package:pnyws/wrappers/mk_navigate.dart';
 
 @immutable
 class SharedCoordinator extends CoordinatorBase {
   const SharedCoordinator(GlobalKey<NavigatorState> navigatorKey) : super(navigatorKey);
+
+  void toHome() {
+    navigator?.pushAndRemoveUntil(
+      MkNavigate.fadeIn(HomePage(), name: MkRoutes.start),
+      (Route<void> route) => false,
+    );
+  }
 
   void toSplash() {
     navigator?.pushAndRemoveUntil(
